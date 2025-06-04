02f1918484989dfde054ac42310c11bd0c315b71
promahbubul <mahbublalam500@gmail.com>
2025-06-05 00:17:07 +0600

    Progress Size element update

--- src/components/elements/progress/ProgressSize/index.tsx
+++ src/components/elements/progress/ProgressSize/index.tsx
@@ -1,6 +1,30 @@
+import CardContainer from "@/components/ui/CardContainer";
+
 const ProgressSize = () => {
+  const codeToString = `<div className="max-w-xl space-y-4">
+  <div className="h-1 w-full bg-[#e9eef5] rounded-full">
+    <div className="w-6/12 h-full bg-[#4f46e5] rounded-full"></div>
+  </div>
+  <div className="h-2 w-full bg-[#e9eef5] rounded-full">
+    <div className="w-6/12 h-full bg-[#4f46e5] rounded-full"></div>
+  </div>
+  <div className="h-3 w-full bg-[#e9eef5] rounded-full">
+    <div className="w-6/12 h-full bg-[#4f46e5] rounded-full"></div>
+  </div>
+  <div className="h-4 w-full bg-[#e9eef5] rounded-full">
+    <div className="w-6/12 h-full bg-[#4f46e5] rounded-full"></div>
+  </div>
+</div>`;
   return (
-    <div>ProgressSize</div>
-  )
-}
-export default ProgressSize
+    <>
+      <CardContainer
+        title={"Progress Size"}
+        description={
+          "The Progress components can have various sizes. Check out code for detail of usage."
+        }
+        jsxCodeString={codeToString}
+      />
+    </>
+  );
+};
+export default ProgressSize;
